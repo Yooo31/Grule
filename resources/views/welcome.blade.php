dff9cdc8f7cab10000082cbdab9574f3e0c22698
Yooo31 <mrts.yoan@gmail.com>
2022-04-25 00:22:00 +0200

    Début du projet GRule !

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+
+@section('title')
+  Bienvenue !
+@endsection
+
+@section('content')
+  <h1 class="title">GRules</h1>
+  <div>
+    <p>Voici le premier texte !</p>
+  </div>
+@endsection
